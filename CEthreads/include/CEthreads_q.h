57ed3c15639720212b141db14831b01ec2349fd8
Joseph Jimenez <josephdjz@hotmail.com>
2024-10-13 15:02:25 -0600

    Delete CEthreads directory

--- CEthreads/include/CEthreads_q.h
+++
@@ -1,7 +0,0 @@
-#include "CEthreads.h"
-void CEthread_q_init(CEthread_private_t *node);
-void CEthread_q_add(CEthread_private_t *node);
-void CEthread_q_delete(CEthread_private_t *node);
-void CEthread_q_state_display();
-void CEthread_q_add_waiter(CEthread_private_t *thread, CEthread_private_t **waiters);
-CEthread_private_t * CEthread_q_search(unsigned long);
