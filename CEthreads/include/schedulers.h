1a90f33ab81dfd2f0cd627fac28ab64682bf84c1
Joseph Jimenez <josephdjz@hotmail.com>
2024-10-13 17:27:52 -0600

    Clean .h of scheduler

--- CEthreads/include/schedulers.h
+++ CEthreads/include/schedulers.h
@@ -22,7 +22,4 @@ void sjf_scheduler(Ship ships[], int ship_count);
 void real_time_scheduler(Ship ships[], int ship_count, float max_time);
 void round_robin_scheduler(Ship ships[], int ship_count, int time_quantum);
 void fcfs_scheduler(Ship ships[], int ship_count);
-void test_schedule(Ship ships[], int ship_count);
-SchedulerType get_scheduler_type(const char* scheduler_name);
-
 #endif
