956b183f2aebf04ac558a5977117816a4b715a27
Joseph Jimenez <josephdjz@hotmail.com>
2024-10-14 06:43:30 -0600

    Pruebas Interfaz

--- CEthreads/src/mock_send_struct.c
+++ CEthreads/src/mock_send_struct.c
@@ -4,78 +4,145 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-// Define the FlowManager struct
+#define MAX_SHIPS 5
+
+typedef enum {
+    NORMAL,
+    PATROL,
+    FISHING
+} ShipType;
+
 typedef struct {
-    int method;
-    int canal_length;
-    int current_direction;
-    int ships_passed;
-    int total_ships_passed;
-    int normal_ship_speed;
-    int fishing_ship_speed;
-    int patrol_ship_speed;
-    int ships_in_queue_LR;
-    int ships_in_queue_RL;
-    int ships_in_midcanal_LR;
-    int ships_in_midcanal_RL;
-    int ships_in_done_LR;
-    int ships_in_done_RL;
-    int w_param;
-    float t_param;
-} FlowManager;
+    ShipType type;          // Type of ship (int)
+    int position;           // Position in canal L->R
+} SimpleShip;
+
+typedef struct {
+    SimpleShip queue_LR_data[MAX_SHIPS];        // Ships L->R
+    SimpleShip queue_RL_data[MAX_SHIPS];        // Ships R->L
+    SimpleShip midcanal_data[MAX_SHIPS];        // Ships in mid-canal
+    SimpleShip done_LR_ships[MAX_SHIPS];        // Completed L->R ships
+    SimpleShip done_RL_ships[MAX_SHIPS];        // Completed R->L ships
+    int canal_length;                   // Length of the canal
+    int actual_direction;               // Current direction
+    int method;                         // Flow control method (int)
+    int scheduler;                      // Scheduler (int)
+} InterfaceData;
 
 int main() {
     // Create a mock serial port
     int serial_fd = open("mock_serial_port", O_RDWR);
+
     if (serial_fd == -1) {
         perror("Unable to open mock serial port");
         return 1;
     }
 
-    // Create a FlowManager struct
-    FlowManager flow_manager;
-    flow_manager.method = 1;
-    flow_manager.canal_length = 10;
-    flow_manager.current_direction = 0;
-    flow_manager.ships_passed = 0;
-    flow_manager.total_ships_passed = 0;
-    flow_manager.normal_ship_speed = 5;
-    flow_manager.fishing_ship_speed = 3;
-    flow_manager.patrol_ship_speed = 2;
-    flow_manager.ships_in_queue_LR = 2;
-    flow_manager.ships_in_queue_RL = 1;
-    flow_manager.ships_in_midcanal_LR = 0;
-    flow_manager.ships_in_midcanal_RL = 0;
-    flow_manager.ships_in_done_LR = 0;
-    flow_manager.ships_in_done_RL = 0;
-    flow_manager.w_param = 2;
-    flow_manager.t_param = 1.5;
+    // Create an InterfaceData struct
+    InterfaceData interface_data;
 
-    // Simulate the sending of the FlowManager struct at different moments of the algorithm
-    printf("Sending initial FlowManager struct...\n");
-    write(serial_fd, &flow_manager, sizeof(FlowManager));
+    // Initial state: all ships at position 0
+    interface_data.canal_length = 5; // Channel length
+    interface_data.actual_direction = 0; // Initial direction (L->R)
+    interface_data.method = 1; // Flow control method
+    interface_data.scheduler = 2; // Scheduler
+
+    // Initialize ships
+    interface_data.queue_LR_data[0].type = NORMAL;
+    interface_data.queue_LR_data[0].position = 0;
+    interface_data.queue_LR_data[1].type = PATROL;
+    interface_data.queue_LR_data[1].position = 0;
+    interface_data.queue_LR_data[2].type = FISHING;
+    interface_data.queue_LR_data[2].position = 0;
+    interface_data.queue_LR_data[3].type = NORMAL;
+    interface_data.queue_LR_data[3].position = 0;
+    interface_data.queue_LR_data[4].type = PATROL;
+    interface_data.queue_LR_data[4].position = 0;
+
+    interface_data.queue_RL_data[0].type = FISHING;
+    interface_data.queue_RL_data[0].position = 0;
+    interface_data.queue_RL_data[1].type = NORMAL;
+    interface_data.queue_RL_data[1].position = 0;
+    interface_data.queue_RL_data[2].type = PATROL;
+    interface_data.queue_RL_data[2].position = 0;
+    interface_data.queue_RL_data[3].type = FISHING;
+    interface_data.queue_RL_data[3].position = 0;
+    interface_data.queue_RL_data[4].type = NORMAL;
+    interface_data.queue_RL_data[4].position = 0;
+
+    interface_data.midcanal_data[0].type = NORMAL;
+    interface_data.midcanal_data[0].position = 0;
+    interface_data.midcanal_data[1].type = PATROL;
+    interface_data.midcanal_data[1].position = 0;
+    interface_data.midcanal_data[2].type = FISHING;
+    interface_data.midcanal_data[2].position = 0;
+    interface_data.midcanal_data[3].type = NORMAL;
+    interface_data.midcanal_data[3].position = 0;
+    interface_data.midcanal_data[4].type = PATROL;
+    interface_data.midcanal_data[4].position = 0;
+
+    interface_data.done_LR_ships[0].type = NORMAL;
+    interface_data.done_LR_ships[0].position = 0;
+    interface_data.done_LR_ships[1].type = PATROL;
+    interface_data.done_LR_ships[1].position = 0;
+    interface_data.done_LR_ships[2].type = FISHING;
+    interface_data.done_LR_ships[2].position = 0;
+    interface_data.done_LR_ships[3].type = NORMAL;
+    interface_data.done_LR_ships[3].position = 0;
+    interface_data.done_LR_ships[4].type = PATROL;
+    interface_data.done_LR_ships[4].position = 0;
+
+    interface_data.done_RL_ships[0].type = FISHING;
+    interface_data.done_RL_ships[0].position = 0;
+    interface_data.done_RL_ships[1].type = NORMAL;
+    interface_data.done_RL_ships[1].position = 0;
+    interface_data.done_RL_ships[2].type = PATROL;
+    interface_data.done_RL_ships[2].position = 0;
+    interface_data.done_RL_ships[3].type = FISHING;
+    interface_data.done_RL_ships[3].position = 0;
+    interface_data.done_RL_ships[4].type = NORMAL;
+    interface_data.done_RL_ships[4].position = 0;
+
+    // Print the size of InterfaceData struct
+    printf("Size of InterfaceData: %lu\n", sizeof(InterfaceData));
+
+    // Send the struct with padding to ensure correct alignment
+    printf("Sending initial InterfaceData struct...\n");
+
+    write(serial_fd, &interface_data, sizeof(InterfaceData));
+
     sleep(2);
 
-    // Update the FlowManager struct to simulate a change in the algorithm
-    flow_manager.ships_in_queue_LR = 1;
-    flow_manager.ships_in_queue_RL = 2;
-    flow_manager.ships_in_midcanal_LR = 1;
-    flow_manager.ships_in_midcanal_RL = 0;
+    // Move ships one position to the right
+    for (int i = 0; i < MAX_SHIPS; i++) {
+        interface_data.queue_LR_data[i].position = (interface_data.queue_LR_data[i].position + 1) % interface_data.canal_length;
+        interface_data.queue_RL_data[i].position = (interface_data.queue_RL_data[i].position + 1) % interface_data.canal_length;
+        interface_data.midcanal_data[i].position = (interface_data.midcanal_data[i].position + 1) % interface_data.canal_length;
+        interface_data.done_LR_ships[i].position = (interface_data.done_LR_ships[i].position + 1) % interface_data.canal_length;
+        interface_data.done_RL_ships[i].position = (interface_data.done_RL_ships[i].position + 1) % interface_data.canal_length;
+    }
 
-    printf("Sending updated FlowManager struct...\n");
-    write(serial_fd, &flow_manager, sizeof(FlowManager));
+    printf("Sending updated InterfaceData struct...\n");
+
+    write(serial_fd, &interface_data, sizeof(InterfaceData));
+
     sleep(2);
 
-    // Update the FlowManager struct to simulate another change in the algorithm
-    flow_manager.ships_in_queue_LR = 0;
-    flow_manager.ships_in_queue_RL = 1;
-    flow_manager.ships_in_midcanal_LR = 0;
-    flow_manager.ships_in_midcanal_RL = 1;
-    flow_manager.ships_in_done_LR = 1;
-    flow_manager.ships_in_done_RL = 0;
+    // Change direction
+    interface_data.actual_direction = 1; // Change direction to R->L
 
-    printf("Sending final FlowManager struct...\n");
-    write(serial_fd, &flow_manager, sizeof(FlowManager));
+    // Move ships one position to the left
+    for (int i = 0; i < MAX_SHIPS; i++) {
+        interface_data.queue_LR_data[i].position = (interface_data.queue_LR_data[i].position - 1 + interface_data.canal_length) % interface_data.canal_length;
+        interface_data.queue_RL_data[i].position = (interface_data.queue_RL_data[i].position - 1 + interface_data.canal_length) % interface_data.canal_length;
+        interface_data.midcanal_data[i].position = (interface_data.midcanal_data[i].position - 1 + interface_data.canal_length) % interface_data.canal_length;
+        interface_data.done_LR_ships[i].position = (interface_data.done_LR_ships[i].position - 1 + interface_data.canal_length) % interface_data.canal_length;
+        interface_data.done_RL_ships[i].position = (interface_data.done_RL_ships[i].position - 1 + interface_data.canal_length) % interface_data.canal_length;
+    }
+
+    printf("Sending final InterfaceData struct...\n");
+
+    write(serial_fd, &interface_data, sizeof(InterfaceData));
 
     // Close the serial port
     close(serial_fd);
